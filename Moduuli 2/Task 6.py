bb39bc34037c27bd77150ddd620ee5885451065c
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-01-15 18:52:23 +0300

    task 6

--- Moduuli 2/Task 6.py
+++ Moduuli 2/Task 6.py
@@ -4,5 +4,15 @@ kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
 nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
 Vihje: tutustu random.randint()-funktion käyttöön.
 """
+import random
 
+print(f"Satunnainen kolminumeroinen koodi on "
+      f"{random.randint(0,9)}"
+      f"{random.randint(0,9)}"
+      f"{random.randint(0,9)}.")
 
+print(f"Satunnainen nelinumeroinen koodi on "
+      f"{random.randint(0,6)}"
+      f"{random.randint(0,6)}"
+      f"{random.randint(0,6)}"
+      f"{random.randint(0,6)}.")
