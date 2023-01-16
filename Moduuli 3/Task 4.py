57aec549205179996234a5282d5b0d7ae09ae58f
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-01-16 16:12:17 +0300

    M3t4

--- Moduuli 3/Task 4.py
+++ Moduuli 3/Task 4.py
@@ -1,3 +1,18 @@
+"""
+Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi.
+Vuosi on karkausvuosi, jos se on jaollinen neljällä.
+Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+"""
 
+Vuosi = float(input("Anna vuosi: "))
 
+if Vuosi % 100 == 0:
+    if Vuosi % 400 == 0:
+        print(f"{Vuosi} on karkausvuosi.")
+    else:
+        print(f"{Vuosi} ei ole karkavuotta.")
+elif Vuosi % 4 == 0:
+    print(f"{Vuosi} on karkausvuosi.")
+else:
+    print(f"{Vuosi} ei ole karkavuotta.")
 
